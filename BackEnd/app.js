f648f6c41df765dd32b5dfc688d73c6399f76870
ashish ranjan <ashishranjanmahi07@gmail.com>
2024-08-13 13:33:20 +0530

    Created dummy get api

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,6 +1,35 @@
 import express from "express";
+import cros from "cors";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+
 
 const app= express();
+
+
+//Middle ware ----- 
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser());
+const corsOption ={
+    origin:"http//localhost:3000",
+    credentials : true
+}
+app.use(cros(corsOption));
+
+
+//Home api 
+//Get 
+//http//localhost:8080/home
+
+app.get("/home" ,(req, res)=>{
+    return res.status(200).json({
+        message : "I am just tesing my first Api",
+        success : true
+    })
+})
 
 const PORT =8080;
 
